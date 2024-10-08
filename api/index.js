8f07d509b765b4e792833acd7b035f5f0122208a
Sachin Yadav <sachinyadav68763@gmail.com>
2024-10-08 19:24:06 +0530

    user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000 , ()=>{
-    console.log("the port is running on 3000")
-})
+app.listen(3000, () => {
+  console.log("The server is running on port 3000");
+});
