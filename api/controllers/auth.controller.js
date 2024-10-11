a8e5dbce01a41022d2dff29c0eb92ae1ac6e90ea
Sachin Yadav <sachinyadav68763@gmail.com>
2024-10-12 03:18:36 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
-export const signup= async(req, res)=>{
+import { errorHandler } from '../utils/error.js';
+export const signup= async(req, res , next)=>{
     const {username , email , password} = req.body ;
     const hashedPassword = bcryptjs.hashSync(password , 10);
     const newUser = new User({username , email , password: hashedPassword});
@@ -11,4 +11,16 @@ export const signup= async(req, res)=>{
    }catch(error){
     next(error);
    }
+}
+
+export const signin = async (req ,res , next)=>{
+    const {email , password} = req.body ;
+    try{
+     const validUser = await User.findOne({email});
+     if(!validUser) return next(errorHandler(404 , 'User not found'));
+     const validPassword = bcryptjs.compareSync(password , validUser.password);
+     if(!validPassword) return next(errorHandler(401 , 'Invalid Password'));
+    }catch(error){
+        next(error)
+    }
 }
--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,25 +1,78 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Signin = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error during signup:', error);
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
-    <h1 className='text-3xl text-center font-semibold my-7'>
-      Sign Up</h1>
-     <form className='flex flex-col gap-4'>
-      <input type='text'placeholder='username' id='username' className='border p-3 rounded-lg'/>
-      <input type='text' placeholder='email' id='email' className='border p-3 rounded-lg'/>
-      <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg'/>
-      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-        Sign up</button> 
-     </form>
-     <div className='flex gap-2 mt-5'>
-      <p>Have an account?</p>
-      <Link to={"/signin"} >
-       <span className='text-blue-700'>Sign in</span>
-      </Link>
-     </div>
+      <h1 className='text-3xl text-center font-semibold my-7'>
+        Sign Up
+      </h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          onChange={handleChange}
+          type='text'
+          placeholder='Username'
+          id='username'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          onChange={handleChange}
+          type='text'
+          placeholder='Email'
+          id='email'
+          className='border p-3 rounded-lg'
+        />
+        <input
+          onChange={handleChange}
+          type='password'
+          placeholder='Password'
+          id='password'
+          className='border p-3 rounded-lg'
+        />
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          Sign Up
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to='/signin'>
+          <span className='text-blue-700'>Sign In</span>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signin 
+export default Signin;
